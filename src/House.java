c8849831739ba5dc805ca674c3cfeb9055dc379d
GAFUR <GAFUR@192.168.0.29>
2019-09-18 17:14:23 -0700

    1-5 Finished House.

--- src/House.java
+++ src/House.java
@@ -2,8 +2,82 @@
 public class House {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JackHouseBuilt();
+		Blank();
+		IsMalt();
+		ThatJackBuilt();
+		Blank();
+		IsRat();
+		AteMalt();
+		ThatJackBuilt();
+		Blank();
+		IsCat();
+		KilledRat();
+		AteMalt();
+		ThatJackBuilt();
+		Blank();
+		IsDog();
+		WorriedCat();
+		KilledRat();
+		AteMalt();
+		ThatJackBuilt();
+		Blank();
+		CowCrumpledHorn();
+		TossedDog();
+		WorriedCat();
+		KilledRat();
+		AteMalt();
+		ThatJackBuilt();
+		Blank();
+		Maiden();
+		MilkedCow();
+		TossedDog();
+		WorriedCat();
+		KilledRat();
+		AteMalt();
+		ThatJackBuilt();
+	}
+	public static void JackHouseBuilt() {
+		System.out.println("This is the house that Jack built.");
+	}
+	public static void Blank() {
+		System.out.println();
+	}
+	public static void IsMalt() {
+		System.out.println("This is the malt");
+	}
+	public static void ThatJackBuilt() {
+		System.out.println("That lay in the house that Jack built.");
+	}
+	public static void IsRat() {
+		System.out.println("This is the rat,");
+	}
+	public static void AteMalt() {
+		System.out.println("That ate the malt");
+	}
+	public static void IsCat() {
+		System.out.println("This is the cat,");
+	}
+	public static void KilledRat() {
+		System.out.println("That killed the rat,");
+	}
+	public static void IsDog() {
+		System.out.println("This is the Dog,");
+	}
+	public static void WorriedCat() {
+		System.out.println("That worried the cat,");
+	}
+	public static void CowCrumpledHorn() {
+		System.out.println("This the cow with the crumpled horn,");
+	}
+	public static void TossedDog() {
+		System.out.println("That tossed the dog,");
+	}
+	public static void Maiden() {
+		System.out.println("This is the maiden all forlorn");
+	}
+	public static void MilkedCow() {
+		System.out.println("That milked the cow with the crumpled horn,");
 	}
 
 }
